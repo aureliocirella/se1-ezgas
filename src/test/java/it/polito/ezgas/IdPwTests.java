891375bba19b0ee3d4852bec7ec04b3e4e898691
behnam263 <behnam263@yahoo.com>
2020-05-18 02:51:31 +0200

    feat: JUnit test methods of IdPw class

--- src/test/java/it/polito/ezgas/IdPwTests.java
+++ src/test/java/it/polito/ezgas/IdPwTests.java
@@ -20,60 +20,72 @@ public class IdPwTests {
 	@Test
 	public void testIdPw1_1(){
 		IdPw gs = new IdPw(); 
-		
 		String user = gs.getUser(); 
 		assertNull(user);
 	}
-	
 	
 	@Test
 	public void testIdPw1_2(){
-		String inputpass = "testpass";
 		IdPw gs = new IdPw(); 
-	 
-		gs.setUser(inputpass);
-		String outputpass = gs.getUser(); 
-		assertTrue(outputpass == inputpass);
+		String pass = gs.getPw(); 
+		assertNull(pass);
 	}
+	
 	
 	@Test
 	public void testIdPw1_3(){
-		IdPw gs = new IdPw(); 
-		
-		String user = gs.getUser(); 
-		assertNull(user);
-	}
-	
-	@Test
-	public void testIdPw1_4(){
-		IdPw gs = new IdPw(); 
-		
-		String user = gs.getPw(); 
-		assertNull(user);
-	}
-	
-	
-	@Test
-	public void testIdPw1_5(){
 		String inputpass = "testpass";
-		IdPw gs = new IdPw(); 
-	 
+		IdPw gs = new IdPw(); 	 
 		gs.setPw(inputpass);
 		String outputpass = gs.getPw(); 
 		assertTrue(outputpass == inputpass);
 	}
 	
 	@Test
-	public void testIdPw1_6(){
-		String inputpass = "testpass";
+	public void testIdPw1_4(){
 		String inputuser = "testuser";
-		IdPw gs = new IdPw(inputuser,inputpass); 
-
-	 
-		assertTrue(gs.getUser() == inputuser);
-		assertTrue(gs.getPw() == inputpass);
+		IdPw gs = new IdPw(); 
+		gs.setUser(inputuser);
+		String outputpass = gs.getUser(); 
+		assertTrue(outputpass == inputuser);
 	}
 	
+
 	
+	@Test
+	public void testIdPw1_5(){
+		String inputuser = "";
+		IdPw gs = new IdPw(); 	 
+		gs.setUser(inputuser);
+		String outputuser = gs.getUser(); 
+		assertTrue(outputuser == inputuser);
+	}
+	@Test
+	public void testIdPw1_6(){
+		String inputuser = null;
+		IdPw gs = new IdPw(); 	 
+		gs.setUser(inputuser);
+		String outputuser = gs.getUser(); 
+		assertNull(outputuser);
+	}
+	
+	@Test
+	public void testIdPw1_7(){
+		String inputpass = "";
+		IdPw gs = new IdPw(); 	 
+		gs.setPw(inputpass);
+		String outputpass = gs.getPw(); 
+		assertTrue(outputpass == inputpass);
+	}
+	@Test
+	public void testIdPw1_8(){
+		String inputpass = null;
+		IdPw gs = new IdPw("testuser",inputpass);  
+		gs.setPw(inputpass);
+		String outputpass = gs.getPw(); 
+		assertNull(outputpass);
+	}
+	
+	 
 	
 }
